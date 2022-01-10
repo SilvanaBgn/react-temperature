21a9ee7e927d84d00df54113cbd91e22788e0986
silvana <silvana.belen.garcia@gmail.com>
2022-01-10 16:46:43 -0300

    Celsius input updates BoilingStatus

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Field from './components/Field';
 import BoilingStatus from './components/BoilingStatus';
+import { useEffect } from 'react/cjs/react.development';
 
 function toCelsius(fahrenheit) {
   return (fahrenheit - 32) * 5 / 9;
@@ -14,17 +15,20 @@ function App() {
   console.log('App');
 
   const [tempC, setTempC] = useState(0);
+  const [tempF, setTempF] = useState(32);
 
   function changeTemperature(newTemp) {
     setTempC(newTemp);
+    console.log('newTemp', newTemp);
+    console.log('this', this);
   }
 
   return (
     <section className="App">
       <h1>Temperature</h1>
       <p><i>Lifting up the children state with Hooks</i></p>
-      <Field unitName = "Celsius" temp = {tempC}/>
-      <Field unitName = "Fahrenheit" temp = {toFahrenheit(tempC)} />
+      <Field unitName = "Celsius" temp = {tempC} changeTemperature = {changeTemperature} />
+      {/* <Field unitName = "Fahrenheit" temp = {toFahrenheit(tempC)} changeTemperature /> */}
       <BoilingStatus celsiusTemperature = {tempC}/>
     </section>
   );
--- src/components/Field.js
+++ src/components/Field.js
@@ -1,12 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Field(props) {
-  const [temp, setTemp] = useState(props.temp)
+  const [temp, setTemp] = useState(props.temp);
+  const changeTemperature = props.changeTemperature;
+  console.log('changeTemperature', changeTemperature)
+
+
+  function handleInputChange(e) {
+    const newTemp = e.target.value;
+    setTemp(newTemp);
+    changeTemperature(newTemp);
+  }
 
   return (
     <fieldset>
        <legend>Enter temperature in {props.unitName}:</legend>
-       <input value = {temp}></input>
+       <input value = {temp}
+              onChange = {handleInputChange} ></input>
     </fieldset>
   )
 }
